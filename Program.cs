8cda8cc9acaa4fed685a6721dd4e0be5164e538a
ialikh72 <ialikh.w1@gmail.com>
2026-03-10 23:26:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+        Console.WriteLine("enter a:");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter b:");
+        b = Convert.ToInt32(Console.ReadLine());
+        Sum(a, b);
+        Sub(a, b);
+        Mul(a, b);
+        Div(a, b);
+
+    }
+    static void Sum(int x, int y)
+    {
+       
+        Console.WriteLine("sum is:"+( x += y));
+    }
+    static void Sub(int x, int y)
+    {
+        Console.WriteLine("sub is:"+(x -= y)) ;
+    }
+    static void Mul(int x, int y)
+    {
+        Console.WriteLine("mul is:"+(x *= y));
+    }
+    static void Div(int x, int y)
+    {
+        validate(x, y);
+        Console.WriteLine("div is:"+(x /= y));
+    }
+    static void validate(int a, int b)
+    {
+        if(b==0)
+            Console.WriteLine("div is not possible");
+        else
+            Console.WriteLine("div is possible");
+    }
+}
